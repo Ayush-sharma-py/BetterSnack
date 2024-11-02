1020b8c6558c9be0bc2142df7e71eaa76cee52a0
Ayush Sharma <ayushsharma14@gmail.com>
2024-11-02 01:13:08 -0500

    Quick fix 2

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c9a81aa7b274151e2e2393a364b00e076cabec5bdba2c126777f06ae9f3967b4
-size 6311
+oid sha256:3d894e9486ab05676b18451c56755de575ae47a85126027cc0ecf6ef7a561e84
+size 6482
