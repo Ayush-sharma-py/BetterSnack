3bdc3418e8b7c7a165a4c3bdc1ed42c6dc15b6d4
Ayush Sharma <ayushsharma14@gmail.com>
2024-11-03 14:45:09 -0600

    Updated Filtering logic + More Vectorization I keep removing code :)

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:809b2161f092fbea3fd5a65c1c10d79f93d8f12184eba79f4820b0b57487f039
-size 9049
+oid sha256:50cc3b3a7842da55df27ea1ed552bc31a9537d731acfef262e78d24b080d1f2a
+size 8853
