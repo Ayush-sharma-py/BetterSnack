2347819ab0c90e89655bb41bd0aa36a9ce47bd0c
Ayush Sharma <ayushsharma14@gmail.com>
2024-12-20 10:51:12 +0530

    fixing parameters

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a82d030775492190c413cf2d09e02d1d7ac72d338160c01dbb3c83f1df8b7d78
-size 9040
+oid sha256:9aefddd02af204890097d90a3c6724d9b9fe4584091bf846230a2e4c35659299
+size 9047
